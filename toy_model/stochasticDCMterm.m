23e81170fb15012707166257f2b5afe6c9155b1d
Kevin Aquino <kevin.aquino@monash.edu>
2018-02-13 16:26:35 +1100

    Adding in stochastic version of the problem

---
+++ toy_model/stochasticDCMterm.m
@@ -0,0 +1,15 @@
+function dW = stochasticDCMterm(t,z,C,inputs)
+
+% Note: this is a function that evaluates the factor infront of the dW term at every instance of t 
+% useful for the stochastic bilinear model, note this is very different to what the deterministic version does instead
+
+% This is a vector adding in Stochastic terms within the inputs
+w_u = ones(size(C,2));
+
+% This is a vector adding in Stochastic terms within each node, i.e. each node has endogenous inputs
+w_z = ones(size(z));
+
+% Adding them all together for the stochastic DCM
+dW = C*w_u + w_z;
+
+end
